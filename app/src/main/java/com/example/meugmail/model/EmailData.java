49b788641090b51fc60bdf4ca9eb9fcb5968fd7a
marco sena <marcoacsena@gmail.com>
2020-09-17 16:34:55 -0300

    Inclusão do do delete email!

--- app/src/main/java/com/example/meugmail/model/EmailData.java
+++ app/src/main/java/com/example/meugmail/model/EmailData.java
@@ -6,28 +6,63 @@ public class EmailData {
         private String mTitle;
         private String mDetails;
         private String mTime;
+        private boolean favoritado = false;
+        private boolean selected;
 
-        public EmailData(String mSender, String mTitle, String mDetails, String mTime) {
-            this.mSender = mSender;
-            this.mTitle = mTitle;
-            this.mDetails = mDetails;
-            this.mTime = mTime;
-        }
+    public EmailData(String mSender, String mTitle, String mDetails, String mTime, boolean favoritado, boolean selected) {
+        this.mSender = mSender;
+        this.mTitle = mTitle;
+        this.mDetails = mDetails;
+        this.mTime = mTime;
+        this.favoritado = favoritado;
+        this.selected = selected;
+    }
 
-        public String getmSender() {
-            return mSender;
-        }
+    public String getmSender() {
+        return mSender;
+    }
 
-        public String getmTitle() {
-            return mTitle;
-        }
+    public void setmSender(String mSender) {
+        this.mSender = mSender;
+    }
 
-        public String getmDetails() {
-            return mDetails;
-        }
+    public String getmTitle() {
+        return mTitle;
+    }
 
-        public String getmTime() {
-            return mTime;
-        }
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
 
+    public String getmDetails() {
+        return mDetails;
+    }
+
+    public void setmDetails(String mDetails) {
+        this.mDetails = mDetails;
+    }
+
+    public String getmTime() {
+        return mTime;
+    }
+
+    public void setmTime(String mTime) {
+        this.mTime = mTime;
+    }
+
+    public boolean isFavoritado() {
+        return favoritado;
+    }
+
+    public void setFavoritado(boolean favoritado) {
+        this.favoritado = favoritado;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
 }
